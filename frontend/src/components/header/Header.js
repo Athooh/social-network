eebe35dbd9fce9c804e27531f7ccef3a025cbf2c
Athooh <s.athooh@gmail.com>
2025-03-03 22:52:29 +0300

    Update header component to include logout functionality and change home link

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,22 +1,32 @@
 // src/components/header/Header.js
+'use client';
+
 import styles from '@/styles/Header.module.css';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    // Add logout logic here
+    router.push('/');
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.headerContent}>
         <div className={styles.logo}>
-          <Link href="/">
+          <Link href="/home">
             <h1>My Social Network</h1>
           </Link>
         </div>
         <nav className={styles.nav}>
-          <Link href="/">Home</Link>
+          <Link href="/home">Home</Link>
           <Link href="/posts">Posts</Link>
           <Link href="/profile/1">Profile</Link>
           <Link href="/messages">Messages</Link>
-          <Link href="/auth/login">Login</Link>
+          <button onClick={handleLogout}>Logout</button>
         </nav>
       </div>
     </header>
