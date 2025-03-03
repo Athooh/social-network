c79698f778af11d1d3760c4b07a2a206bbc6bf9b
Athooh <s.athooh@gmail.com>
2025-03-03 14:33:11 +0300

    Add initial styles and components for posts, groups, messages, and user profiles

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,7 +1,9 @@
-// src/app/page.js
+import Header from '@/components/header/Header';
+
 export default function Home() {
   return (
     <div>
+      <Header/>
       <h1>Welcome to My Social Network</h1>
       <img src="/logo.png" alt="Logo" />
     </div>
