d4684b5449f9870892d9ff21151bdccaa746edf7
Raymond9734 <rymadara97@gmail.com>
2025-03-17 19:12:21 +0300

    Remove console log from CreatePost component to reduce unnecessary output during post creation.

--- frontend/src/components/posts/CreatePost.js
+++ frontend/src/components/posts/CreatePost.js
@@ -11,7 +11,6 @@ export default function CreatePost() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Creating post:', { postText, files: selectedFiles });
     setPostText('');
     setSelectedFiles([]);
     setPreviewUrls([]);
