079446a97592ca306e0f805cae91eecfb06296b4
rcaleb <calebmadara58@gmail.com>
2025-03-17 16:02:05 +0300

    Add environment variable configuration for API URL in Next.js setup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
+import { resolve } from 'path';
+import { config } from 'dotenv';
+
+config({ path: resolve('../.env') });
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL || "http://localhost:8080/api", 
+  },
+};
 
 export default nextConfig;
