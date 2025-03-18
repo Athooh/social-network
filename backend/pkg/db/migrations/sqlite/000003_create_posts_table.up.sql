972a6ddaa8e64687a3197fa2ad37ef418394f45a
Raymond9734 <rymadara97@gmail.com>
2025-03-18 09:04:09 +0300

    Add SQL migration files for posts table
    - Created the 'up' migration to establish the posts table with necessary fields.
    - Added the 'down' migration to drop the posts table if it exists.

---
+++ backend/pkg/db/migrations/sqlite/000003_create_posts_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS posts (
+    id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    content TEXT NOT NULL,
+    image_path TEXT,
+    privacy TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
