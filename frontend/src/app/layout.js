488b4d6e2665d1e9c17b1b9195168fdf278bdfbb
Raymond9734 <rymadara97@gmail.com>
2025-03-18 21:46:16 +0300

    Add ToastContainer component to RootLayout for improved user notifications

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { AuthProvider } from "@/context/authcontext";
+import ToastContainer from "@/components/ui/ToastContainer";
 
 export const metadata = {
   title: "NoteBook",
@@ -10,13 +11,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link 
-          rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         />
       </head>
       <body>
         <AuthProvider>{children}</AuthProvider>
+        <ToastContainer />
       </body>
     </html>
   );
