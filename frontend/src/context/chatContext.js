96da1e7d8e9828862d6c3d01736da1ac76f65bcd
Raymond9734 <rymadara97@gmail.com>
2025-03-24 21:08:08 +0300

    feat: create ChatContext and ChatProvider for managing chat state and loading contacts

---
+++ frontend/src/context/chatContext.js
@@ -0,0 +1,55 @@
+"use client";
+
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { useChatService } from "@/services/chatService";
+import { useAuth } from "@/context/authcontext";
+
+const ChatContext = createContext(null);
+
+export const ChatProvider = ({ children }) => {
+  const chatService = useChatService();
+  const { currentUser } = useAuth();
+  const [contacts, setContacts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
+
+  // Initialize chat service when the component mounts
+  useEffect(() => {
+    if (currentUser?.id) {
+      chatService.initializeWebSocketSubscriptions();
+    }
+  }, [currentUser, chatService]);
+
+  // Function to load contacts data - will be called explicitly from the Messages page
+  const loadChatData = async () => {
+    if (isDataLoaded || !currentUser?.id) return;
+
+    setLoading(true);
+    try {
+      const contactsList = await chatService.loadContacts();
+      console.log("contactsList", contactsList);
+      setContacts(contactsList);
+      setIsDataLoaded(true);
+    } catch (error) {
+      console.error("Error loading contacts:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <ChatContext.Provider
+      value={{
+        ...chatService,
+        contacts,
+        loading,
+        loadChatData,
+        isDataLoaded,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChatContext = () => useContext(ChatContext);
