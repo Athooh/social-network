9289e7c92c85301e9e52ef5cecf5a86ea5cfb40a
Raymond9734 <rymadara97@gmail.com>
2025-03-25 09:58:16 +0300

    feat: add HasValidSession method to SessionStore interface for session validation

--- backend/pkg/session/models.go
+++ backend/pkg/session/models.go
@@ -14,4 +14,5 @@ type SessionStore interface {
 	DeleteUserSessions(userID string) error
 	CleanExpired() error
 	GetUserSessions(userID string) ([]models.Session, error)
+	HasValidSession(userID string) (bool, error)
 }
