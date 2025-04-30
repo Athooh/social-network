01fa4b3e41dd4dd73319127e3a235f7b50cfb50a
Raymond9734 <rymadara97@gmail.com>
2025-04-30 17:33:17 +0300

    fix: enforce SenderID as not null in Notification struct for data integrity

--- backend/pkg/models/dbTables/notificationTable.go
+++ backend/pkg/models/dbTables/notificationTable.go
@@ -8,7 +8,7 @@ import (
 type Notification struct {
 	ID            int64          `db:"id,pk,autoincrement"`
 	UserID        string         `db:"user_id,notnull" index:"idx_notification_user_id" references:"users(id) ON DELETE CASCADE"` // Recipient
-	SenderID      sql.NullString `db:"sender_id" references:"users(id) ON DELETE CASCADE"`                                        // Optional sender
+	SenderID      sql.NullString `db:"sender_id,notnull" references:"users(id) ON DELETE CASCADE"`                                // Optional sender
 	Type          string         `db:"type,notnull"`                                                                              // e.g., follow_request, group_invite
 	Message       string         `db:"message,notnull"`                                                                           // Notification message
 	IsRead        bool           `db:"is_read,notnull,default=false"`                                                             // Read status
