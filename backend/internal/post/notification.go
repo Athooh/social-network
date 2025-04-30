7264d454ef713c35455fea4eebcaf7c6824bf159
Raymond9734 <rymadara97@gmail.com>
2025-04-30 17:33:02 +0300

    feat: add SendCommentNotificationToOwner method to notify users of comments on their posts

--- backend/internal/post/notification.go
+++ backend/internal/post/notification.go
@@ -1,19 +1,33 @@
 package post
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	"time"
 
+	notifications "github.com/Athooh/social-network/internal/notifcations"
+	"github.com/Athooh/social-network/pkg/logger"
 	models "github.com/Athooh/social-network/pkg/models/dbTables"
+	"github.com/Athooh/social-network/pkg/user"
 	"github.com/Athooh/social-network/pkg/websocket"
 	"github.com/Athooh/social-network/pkg/websocket/events"
 )
 
 type NotificationService struct {
-	hub *websocket.Hub
+	hub              *websocket.Hub
+	userRepo         user.Repository
+	log              *logger.Logger
+	notificationSRVC notifications.Service
 }
 
-func NewNotificationService(hub *websocket.Hub) *NotificationService {
-	return &NotificationService{hub: hub}
+func NewNotificationService(hub *websocket.Hub, userRepo user.Repository, notifcationSRVC notifications.Service, log *logger.Logger) *NotificationService {
+	return &NotificationService{
+		hub:              hub,
+		userRepo:         userRepo,
+		log:              log,
+		notificationSRVC: notifcationSRVC,
+	}
 }
 
 func (s *NotificationService) NotifyPostCreated(post interface{}, userID, userName string) error {
@@ -158,4 +172,56 @@ func (s *NotificationService) NotifyPostsCommentUpdate(userID string, statsType 
 	s.hub.BroadcastToUser(userID, event)
 
 	return nil
+}
+
+// SendCommentNotification sends a WebSocket notification when a user comments on a post
+func (s *NotificationService) SendCommentNotificationToOwner(userID, commenterID string) {
+	if s.hub == nil {
+		s.log.Warn("WebSocket hub is nil, cannot send comment notification")
+		return
+	}
+
+	// Fetch commenter details
+	commenter, err := s.userRepo.GetByID(commenterID)
+	if err != nil {
+		s.log.Error("Failed to fetch commenter details: %v", err)
+		return
+	}
+	commenterName := commenter.FirstName + " " + commenter.LastName
+
+	// Create notification in database
+	notification := &notifications.NewNotification{
+		UserId:          userID,
+		NotficationType: "comment",
+		SenderId:        sql.NullString{String: commenterID, Valid: true},
+		Message:         fmt.Sprintf("%s commented on your post.", commenterName),
+	}
+	if err := s.notificationSRVC.CreateNotification(notification); err != nil {
+		s.log.Error("Failed to create comment notification: %v", err)
+		return
+	}
+
+	// Retrieve the newly created notification to get its ID and CreatedAt
+	notifications, err := s.notificationSRVC.GetNotifications(userID, 1, 0)
+	if err != nil || len(notifications) == 0 {
+		s.log.Error("Failed to retrieve newly created notification: %v", err)
+		return
+	}
+	dbNotification := notifications[0]
+
+	// Create WebSocket event
+	event := events.Event{
+		Type: events.HeaderNotificationUpdate,
+		Payload: map[string]interface{}{
+			"id":           dbNotification.ID,
+			"type":         "comment",
+			"senderName":   commenterName,
+			"senderAvatar": commenter.Avatar,
+			"message":      notification.Message,
+			"createdAt":    dbNotification.CreatedAt.Format(time.RFC3339),
+			"isRead":       dbNotification.IsRead,
+		},
+	}
+
+	s.hub.BroadcastToUser(userID, event)
 }
