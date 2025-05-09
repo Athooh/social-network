d52c2a9fe76c8c2ce4ef418f31ecdc291a0a31ee
andyosyndoh <andyovvo8@gmail.com>
2025-05-09 09:35:56 +0300

    Chore: created group service to handle requests

---
+++ frontend/src/services/groupService.js
@@ -0,0 +1,34 @@
+import { useAuth } from "@/context/authcontext";
+import { showToast } from "@/components/ui/ToastContainer";
+import { useState, useEffect, useCallback } from "react";
+import { useWebSocket, EVENT_TYPES } from "./websocketService";
+import { BASE_URL } from "@/utils/constants";
+
+export const useGroupService = () => {
+    const { authenticatedFetch } = useAuth();
+
+    const createGroup = async (formData) => {
+        try {
+            const response = await authenticatedFetch("groups", {
+                method: "POST",
+                body: formData,
+            });
+
+            console.log("Response from createGroup:", response);
+            if (!response.ok) {
+                const errorData = await response.json().catch(() => ({}));
+                throw new Error(
+                    errorData.message || errorData.error || "Failed to create group"
+                );
+            }
+
+            const data = await response.json();
+
+            return data;
+        } catch (error) {
+            console.error("Error creating group:", error);
+            showToast(error.message || "Error creating group", "error");
+        }
+    }
+    return { createGroup };
+}
