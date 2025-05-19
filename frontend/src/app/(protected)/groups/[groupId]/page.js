6022a5645cc437ebe6ed39cde351819db98e165e
andyosyndoh <andyovvo8@gmail.com>
2025-05-19 14:54:22 +0300

    chore:  use whole group

--- frontend/src/app/(protected)/groups/[groupId]/page.js
+++ frontend/src/app/(protected)/groups/[groupId]/page.js
@@ -87,7 +87,7 @@ export default function GroupPostPage() {
       case 'photos':
         return <GroupPhotos posts={posts} />;
       case 'GroupMembers':
-        return <GroupMembers groupId={groupId} />;
+        return <GroupMembers group={group} />;
       case 'GroupEvents':
         return <GroupEvents groupId={groupId} />;
       // Add to renderContent switch
